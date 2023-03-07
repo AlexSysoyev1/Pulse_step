92cb61e6b7843983e9b4b6461b1b6d88cff68b0f
AlexSysoyev1 <111682779+AlexSysoyev1@users.noreply.github.com>
2023-03-07 18:49:39 +0300

    Update smart.php

--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -13,13 +13,13 @@ $mail->CharSet = 'utf-8';
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'alexander.sysoyev1@gmail.com';                 // Наш логин
+$mail->Username = 'a1@gmail.com';                 // Наш логин
 $mail->Password = '**** **** **** ****';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
-$mail->setFrom('alexander.sysoyev1@gmail.com', 'Pulse');   // От кого письмо 
-$mail->addAddress('alador@yandex.ru');     // Add a recipient
+$mail->setFrom('a1@gmail.com', 'Pulse');   // От кого письмо 
+$mail->addAddress('a2@yandex.ru');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
